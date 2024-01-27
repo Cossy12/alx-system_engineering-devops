84b0104463a7711175c290d1582c149d040de224
Cossy12 <cosmasmutuku001@gmail.com>
2024-01-27 09:23:07 -0800

    debugging_4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+#  more amount of traffic an Nginx server can handle.
+# more in unlimited  of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+# Reboot Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
